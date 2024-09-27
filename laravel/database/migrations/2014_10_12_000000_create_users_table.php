5350f70d7d022ec7d42a310c15c5592da205913a
aryansaputra69 <ariyansaputra726@gmail.com>
2024-09-27 17:26:45 +0700

    Latihan Modul 3

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,25 +7,28 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
-     */
-    public function up(): void
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('level')->default('user');
+            $table->string('jenis_kelamin');
             $table->timestamps();
         });
     }
-
     /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
     {
         Schema::dropIfExists('users');
     }
